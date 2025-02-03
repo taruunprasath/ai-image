1f349ade58d5efbfec5d3717263147f0f7c2d9e6
Taruun Prasath <taruunprasathofficial@gmail.com>
2025-02-03 18:07:13 +0530

    changed ui

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-indigo-600 text-white py-4">
+    <header className="bg-gray-800 text-white py-4 shadow-lg shadow-blue-600/50">
       <div className="max-w-screen-xl mx-auto flex justify-center items-center px-4">
-        
         <div className="flex items-center space-x-3">
-          <h1 className="text-3xl font-bold">Your App Title</h1>
+          <h1 className="text-3xl font-bold">AI Image Generator</h1>
         </div>
       </div>
     </header>
